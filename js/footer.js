9777a5432bf89111bb52b524a84f25dd2150ce45
Rundll86 <2028673870@qq.com>
2023-12-17 19:16:37 +0800

    update

--- js/footer.js
+++
@@ -1,3 +0,0 @@
-document.write("<div class=\"footer\">");
-document.write("	<p>© 2022-2023 <a href=\"https://GalaxyCodingStudios.github.io\" class=\"footer-a\" style=\"color:#fff\">银河编程工作室</a> 版权所有</p>");
-document.write("</div>");
