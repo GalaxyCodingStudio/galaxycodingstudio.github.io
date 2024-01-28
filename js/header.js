a763e669325b1d3090c824259d3e465b682b07ac
daiqitao <127941036+daiqitao@users.noreply.github.com>
2024-01-28 15:16:48 +0800

    Add files via upload

---
+++ js/header.js
@@ -0,0 +1,14 @@
+document.write("	<div class=\"screen\">");
+document.write("		<nav class=\"navbar navbar-expand-lg nav\">");
+document.write("			<div class=\"container-fluid\">");
+document.write("				<div class=\"collapse navbar-collapse\">");
+document.write("					<ul class=\"navbar-nav\">");
+document.write("						<li class=\"nav-item\"><a class=\"navbar-brand\" href=\"\/\"><img src=\"\/img\/图片1.png\" alt=\"logo\" alt=\"银河编程网\" width=\"130\" title=\"首页\"><\/a><\/li>");
+document.write("                        <li class=\"nav-item\"><button class=\"btn btn-outline-secondary2\" onclick=\"location.href='/about'\">关于我们<\/button><\/li>");
+document.write("                        <li class=\"nav-item\"><button class=\"btn btn-outline-secondary2\" onclick=\"location.href='/qf'\">赞助银河编程<\/button><\/li>");
+document.write("				    <\/ul>");
+document.write("				<\/div>");
+document.write("	        <\/div>");
+document.write("       <\/nav>");
+document.write("	<\/div>");
+document.write("	<div class=\"i\"><\/div>");
---
+++ js/not.js
@@ -0,0 +1,3 @@
+function jubao(){
+  alert('举报方式：发送邮件给GalaxyGZS@163.com，主题写“举报信”，内容写“我举报https://galaxycodingstudio.github.io/project/xxx，Ta涉嫌xxxxxxxx”。（若格式不正确，我们将无法收到您的举报信！！！另外，请不要重复发信，否则将封ip警告2天）')
+}
